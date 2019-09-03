965ac5494b6e48e07b5b540a4d1512eb0cd5f691
cyberked <37407401+cyberked@users.noreply.github.com>
2019-09-03 04:00:58 -0700

    Update array2.py

--- array2.py
+++ array2.py
@@ -1,17 +1,14 @@
 list1 = list(map(int,input().split(' ')))
 list2 = list(map(int,input().split(' ')))
-biglist = list1 + list2
-biglist.sort()
-def middle(a):
-    split = (len(a))% 2
+a = list1 + list2
+a.sort()
+split = (len(a))% 2
     if (split != 0):
         add = a[split] + a[split+1]
-        return add
+        print(add)
     else:
         add2 = a[split] + a[split-1]
-        return add2
-output = middle(biglist)
-print(output)
+        print(add2)
 
 
 
