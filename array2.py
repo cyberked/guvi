261577dd97b268c2e3ee38ca1829081353281956
cyberked <37407401+cyberked@users.noreply.github.com>
2019-09-03 04:06:29 -0700

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,18 @@
+list1 = list(map(int,input().split(' ')))
+list2 = list(map(int,input().split(' ')))
+biglist = list1 + list2
+biglist.sort()
+def middle(a):
+    split = (len(a))% 2
+    if (split != 0):
+        add = a[split] + a[split+1]
+        return add
+    else:
+        add2 = a[split] + a[split-1]
+        return add2
+    
+output = middle(biglist)
+print(output)
+
+
+
